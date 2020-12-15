103664ff66061130cfabc937ebce32fb36dbc3e6
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-12-15 01:06:09 -0500

    refactor: Configuracion de variables de entorno para Dev [Strapi_MySql]

--- Strapi_mysql/config/database.js
+++ Strapi_mysql/config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'Strapi_mysql'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'Smavo978554490'),
-        ssl: env.bool('DATABASE_SSL', false),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT' | 3306),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
       },
       options: {}
     },
--- Strapi_mysql/config/server.js
+++ Strapi_mysql/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST'),
+  port: env.int('PORT'),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '22f4e4bb63e6c4823a672c2d802583d4'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
