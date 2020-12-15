f541f46ff89642eb69dfcd14fdf703c8e207e561
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-12-14 19:24:38 -0500

    add Strapi_MongoDB - Example [Blog, Agentes, Categoria]

---
+++ Strapi_MongoDB/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c74b6284d3a1134075d751494de4109c'),
+    },
+  },
+});
---
+++ Strapi_MongoDB/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eb24a255-f0fb-4af0-b547-6c49e0967938'
+};
